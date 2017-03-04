058159fdcca03869fa040d833da94f6a5d9384db
Thiago Nascimento <thiagonascimento@MacBook-Air-de-Thiago.local>
2017-03-03 23:25:04 -0300

    moip token

--- descontosveg/moip/views.py
+++ descontosveg/moip/views.py
@@ -4,7 +4,7 @@ from moip import MoIP
 
 def moipSend(request):
     moip = MoIP(razao="Razao",valor="150.00")
-    moip.set_credenciais(token="seu_token",key="sua_key") 
+    moip.set_credenciais(token="IZXOTSU5G1ZXWZQRO4ZCDOOXGDWPBRTE",key="3T366IZHB8F7YZ22PMCJW5UXZNCDAXU7JVYH8IZY") 
     moip.envia()
     resposta = moip.get_resposta()
     return HttpResponse("Token retornado: "+resposta['token'])
