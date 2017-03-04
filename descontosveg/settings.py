8ec0d49c4d1f3f02b9cae0ec723c1d473d901514
Thiago Nascimento <thiagonascimento@MacBook-Air-de-Thiago.local>
2017-03-04 00:55:49 -0300

    contact

--- descontosveg/settings.py
+++ descontosveg/settings.py
@@ -18,7 +18,8 @@ PROJECT_DIR = Path(__file__).parent
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/app-messages'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
