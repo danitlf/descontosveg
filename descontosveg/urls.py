aec376b6f4a956166d5768706861afd3892dcfc0
Thiago Nascimento <thiagonascimento@MacBook-Air-de-Thiago.local>
2017-03-04 00:28:47 -0300

    contact

--- descontosveg/urls.py
+++ descontosveg/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^moip/$', view_moip.moipSend, name='moipSend'),
     url(r'^contact/$', views.contact, name='contact'),
-    url(r'^send_email$', views.send_email, name='send_email'),
+    url(r'^send_email/$', views.send_email, name='send_email'),
 
 ]
 
