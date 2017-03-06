7554b9e6d7d605542e66d69c4844979d1c8443da
Thiago Nascimento <thiagonascimento@MacBook-Air-de-Thiago.local>
2017-03-06 19:55:55 -0300

    purchase

--- descontosveg/moip/models.py
+++ descontosveg/moip/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
+from django.utils.translation import ugettext_lazy as _
 
 from django.db import models
 
 # Create your models here.
+
+
+class Purchase(models.Model):
+    
+    user = models.ForeignKey('book.User')
+    name = models.CharField(_('titulo'),max_length=100)
+    state = models.CharField(_('status'),max_length=200)
+    token = models.CharField(_('token'),max_length=300)
+
+    
