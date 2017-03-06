58fd498de5850ab23714719dea990dcd2d399b0f
Thiago Nascimento <thiagonascimento@MacBook-Air-de-Thiago.local>
2017-03-06 20:05:56 -0300

    purchase

--- descontosveg/moip/models.py
+++ descontosveg/moip/models.py
@@ -7,9 +7,11 @@ from django.utils.translation import ugettext_lazy as _
 
 class Purchase(models.Model):
     
-    user = models.ForeignKey('book.User')
-    name = models.CharField(_('titulo'),max_length=100)
+    user = models.ForeignKey('book.Person')
+    name = models.CharField(_('nome'),max_length=100)
     state = models.CharField(_('status'),max_length=200)
     token = models.CharField(_('token'),max_length=300)
 
     
+
+		
