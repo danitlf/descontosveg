25e2cefe989ad98b15a2c41ff12749d227c585d3
danitlf <danilimah18@gmail.com>
2017-03-08 23:32:53 -0300

    retorno

--- descontosveg/moip/models.py
+++ descontosveg/moip/models.py
@@ -7,16 +7,18 @@ from django.utils.translation import ugettext_lazy as _
 
 class Purchase(models.Model):
     
-    user = models.ForeignKey('book.Person')
+    user = models.ForeignKey('book.Person', null=True, blank=True)
     name = models.CharField(_('nome'),max_length=100)
+    tipo_pagamento = models.CharField(_('tipo_pagamento'),max_length=100, null=True, blank=True)
+    forma_pagamento = models.CharField(_('forma_pagamento'),max_length=100, null=True, blank=True)
     state = models.CharField(_('status'),max_length=200)
+    value = models.CharField(_('value'),max_length=200, null=True, blank=True)
     token = models.CharField(_('token'),max_length=300)
-
+    id_moip =  models.CharField(_('id_moip'),max_length=200, null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    
 class User_Sales(models.Model):
 	sale = models.ForeignKey('book.Sale')
 	state = models.CharField(_('status'),max_length=200)
 	user = models.ForeignKey('book.Person')
 	purchase = models.ForeignKey('Purchase')
-    
-
-		
--- descontosveg/moip/views.py
+++ descontosveg/moip/views.py
@@ -3,21 +3,31 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from moip import MoIP
 from descontosveg.book.models import Book
+from descontosveg.moip.models import Purchase, User_Sales
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import json
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 
 
 def moipSend(request, id_book):
     book_selecionado = Book.objects.get(id=id_book)
     value = str(book_selecionado.value)
     razao = book_selecionado.name
+
+    #creating obj of Purchase
+    purchase = Purchase(name=book_selecionado.name, state="0", value=book_selecionado.value)
+    purchase.save()
+
+
     moip = MoIP(razao=razao,valor=value)
     moip.set_credenciais(token="IZXOTSU5G1ZXWZQRO4ZCDOOXGDWPBRTE",key="3T366IZHB8F7YZ22PMCJW5UXZNCDAXU7JVYH8IZY") 
+    moip.set_id_proprio(str(purchase.id))
     moip.envia()
     resposta = moip.get_resposta()
+
     return HttpResponseRedirect("https://desenvolvedor.moip.com.br/sandbox/Instrucao.do?token="+str(resposta['token']))
 
 
@@ -30,9 +40,27 @@ def moipResponse(request):
     if request.method == "POST":
         print request.data
         data = request.data
-        arquivo = open('retorno_moip.txt', 'w')
-        arquivo.write(str(data))
-        arquivo.close()
+
+        atualiza_compra(data)
+
+        
+        # teste de recebimento de dados do MoIP
+        # arquivo = open('retorno_moip.txt', 'w')
+        # arquivo.write(str(data))
+        # arquivo.close()
+
+
         return HttpResponse()
     else:
         return HttpResponse()
+
+
+#com o retorno do moip nos atualizamos o registro
+def atualiza_compra(dados):
+    compra = Purchase.objects.get(id=dados["id_transacao"][0])
+    compra.forma_pagamento = dados["forma_pagamento"][0]
+    compra.tipo_pagamento = dados["tipo_pagamento"][0]
+    compra.state = dados["status_pagamento"][0]
+    compra.date = datetime.strptime(dados["status_data"][0], "%Y/%m/%d-%H:%M:%S")
+    compra.id_moip = dados["status_data"][0]
+    compra.save()
