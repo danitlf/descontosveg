c01b99b85b50d25a690643b87e2f7eb898a480fd
danitlf <danilimah18@gmail.com>
2017-03-08 21:55:49 -0300

    retorno

--- descontosveg/settings.py
+++ descontosveg/settings.py
@@ -55,10 +55,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
-    'PAGE_SIZE': 10
+    'DEFAULT_PERMISSION_CLASSES': [],
 }
 
 MIDDLEWARE = [
