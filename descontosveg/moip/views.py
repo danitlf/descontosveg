118d3c59b4d22240137dda33aecef05b3ccc21ac
Thiago Nascimento <thiagonascimento@MacBook-Air-de-Thiago.local>
2017-03-13 20:01:55 -0300

    user sales

--- descontosveg/moip/views.py
+++ descontosveg/moip/views.py
@@ -81,7 +81,7 @@ def insere_ofertas_do_usuario(compra):
         id_user_sale = str(sale.id) + str(person.id) + str(compra.id_moip)
         
         #salva na tabela as ofertas do usuario
-        user_sale = User_Sales(sale=sale, user=compra.user, state="1", purchase=compra, id_user_sale=)
+        user_sale = User_Sales(sale=sale, user=compra.user, state="1", purchase=compra, id_user_sale=id_user_sale)
         user_sale.save()
 
 
