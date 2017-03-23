2a1fa4495fe1166d021109bdf029b2a16f361c52
daniel.freitas <daniel.freitas@corp.globo.com>
2017-03-22 21:43:42 -0300

    reset de senha

--- descontosveg/book/admin.py
+++ descontosveg/book/admin.py
@@ -1,6 +1,12 @@
+# coding: utf-8
 from django.contrib import admin
 from descontosveg.book.models import Book, Sale, Partner
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from random import randint
+from django.core.mail import send_mail, BadHeaderError
+
+
 
 
 
@@ -57,12 +63,31 @@ class PartnerAdmin(admin.ModelAdmin):
     
 admin.site.register(Partner, PartnerAdmin)
 
+def reset_password(modeladmin, request, queryset):
+    for usuario in queryset:
+        senha = str(randint(100000,999999))
+        usuario.set_password(senha)
+        usuario.save()
+        #envia email para o descontos veg e para o usuario informando a nova senha
+        envia_email(usuario, senha)
+
+reset_password.short_description = "Resetar senha"
+
+class MyUserAdmin(UserAdmin):
+    actions = [reset_password]
+
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
 
 
 
+def envia_email(usuario, nova_senha):
+    message = "Você solicitou uma nova senha no vegdescontos. \n Sua nova senha é: "+ nova_senha
+    try:
+        send_mail("VegDescontos" ,message , 'descontosveg@gmail.com', ['descontosveg@gmail.com',usuario.email])
 
-
-
-
-
+        print "sucesso"
+    except BadHeaderError:
+        print "falha"
 
